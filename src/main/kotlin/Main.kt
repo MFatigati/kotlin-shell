3cdef7cd8f175adda05bac3b7958f428b11f5b14
Michael Fatigati <fatigati.michael@gmail.com>
2024-12-19 15:23:56 -0800

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,36 @@
 import kotlin.system.exitProcess
 
-val validCommands = arrayOf("exit", "echo")
+val validCommands = arrayOf("exit", "echo", "type")
+
+fun getSecondArgument(input: String): String {
+    return input.split(" ").drop(1)[0]
+}
+
+fun getEverythingExceptFirstArg(input: String): String {
+    return input.split(" ").drop(1).joinToString(" ")
+}
 
 fun main() {
     while (true) {
         print("$ ")
         val input: String = readln() // Wait for user input
         val command = input.split(" ")[0]
-        val remainder = input.split(" ").drop(1).joinToString(" ")
+
         if (command in validCommands) {
             when (command) {
                 "exit" -> exitProcess(0)
-                "echo" -> print(remainder + "\n")
+                "echo" -> {
+                    val remainder = getEverythingExceptFirstArg(input)
+                    print(remainder + "\n")
+                }
+                "type" -> {
+                    val secondArg = getSecondArgument(input)
+                    if (secondArg in validCommands) {
+                        print("$secondArg is a shell builtin\n")
+                    } else {
+                        print("$secondArg: not found\n")
+                    }
+                }
             }
         } else {
             print("$command: command not found\n")
