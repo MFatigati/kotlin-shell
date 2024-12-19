d3fd0546624f0acf94f679a1070cbdeb779d994c
Michael Fatigati <fatigati.michael@gmail.com>
2024-12-19 15:08:24 -0800

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,18 @@
 import kotlin.system.exitProcess
 
-val validCommands = arrayOf("exit")
+val validCommands = arrayOf("exit", "echo")
 
 fun main() {
     while (true) {
         print("$ ")
         val input: String = readln() // Wait for user input
         val command = input.split(" ")[0]
+        val remainder = input.split(" ").drop(1).joinToString(" ")
         if (command in validCommands) {
-            exitProcess(0)
+            when (command) {
+                "exit" -> exitProcess(0)
+                "echo" -> print(remainder + "\n")
+            }
         } else {
             print("$command: command not found\n")
         }
