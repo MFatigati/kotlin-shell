5333f33f759f4a75e721bc10910ccc12a37ef71b
Michael Fatigati <fatigati.michael@gmail.com>
2024-12-19 16:54:46 -0800

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
+import java.io.File
 import kotlin.system.exitProcess
 
-val validCommands = arrayOf("exit", "echo", "type")
+val shellBuiltIns = arrayOf("exit", "echo", "type")
 
 fun getSecondArgument(input: String): String {
     return input.split(" ").drop(1)[0]
@@ -10,13 +11,20 @@ fun getEverythingExceptFirstArg(input: String): String {
     return input.split(" ").drop(1).joinToString(" ")
 }
 
+fun fileExistsInDir(filesDir: String, fileName: String): Boolean {
+    var file = File(filesDir, fileName)
+    return file.exists()
+}
+
 fun main() {
     while (true) {
         print("$ ")
         val input: String = readln() // Wait for user input
         val command = input.split(" ")[0]
+        val path = System.getenv("PATH")
+        val commandFullLocations = path.split(":")
 
-        if (command in validCommands) {
+        if (command in shellBuiltIns) {
             when (command) {
                 "exit" -> exitProcess(0)
                 "echo" -> {
@@ -25,9 +33,22 @@ fun main() {
                 }
                 "type" -> {
                     val secondArg = getSecondArgument(input)
-                    if (secondArg in validCommands) {
+                    var isBuiltIn = secondArg in shellBuiltIns
+                    var found = false
+                    if (isBuiltIn) {
                         print("$secondArg is a shell builtin\n")
-                    } else {
+                    }
+                    if (!isBuiltIn) {
+                        for (dir in commandFullLocations) {
+                            if (fileExistsInDir(dir, secondArg)) {
+                                val location = "${dir}/${secondArg}"
+                                print("$secondArg is $location\n")
+                                found = true
+                                break
+                            }
+                        }
+                    }
+                    if (!isBuiltIn && !found) {
                         print("$secondArg: not found\n")
                     }
                 }
